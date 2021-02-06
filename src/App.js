c7cddd661bdf0b3eeea3f514338bbfae9abb8e37
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-02-06 13:50:44 +0100

    redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
-import ListFilms from "./pages/listfilms/listfilms";
-import AddFilm from "./pages/addfilm/addfilm";
 
 const App = ()=> {
   return (
-       <BrowserRouter>
-         <Switch>
-             <Route exact path="/">
-                 <Redirect to="/listfilms"/>
-             </Route>
-            <Route path={'/listfilms'} component={ListFilms}/>
-            <Route path={'/addfilm'} component={AddFilm}/>
-         </Switch>
-       </BrowserRouter>
+       <div>
+           <h1>Hi</h1>
+       </div>
   );
 }
 
