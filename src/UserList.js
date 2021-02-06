8238384b8bdad64b4ea3ab562f19deed9c0b63d7
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-02-06 14:19:53 +0100

    redux

--- src/UserList.js
+++ src/UserList.js
@@ -1,8 +1,22 @@
+import {useEffect, useState} from "react";
+import API from "./api";
 
 const UserList = ()=>{
+    const [listOfUser,setListOfUser]=useState([])
+
+    useEffect(()=>{
+     API.get('users')
+         .then(res=>{
+         setListOfUser(res.data);
+     }).catch(err=> {
+         console.log(err)
+     })
+    },[])
+
     return(
         <div>
-            <h1>UserList</h1>
+            <h1>List user</h1>
+
         </div>
     )
 }
