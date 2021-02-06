829d52b4f6d0d991fa09a98f1fc9d654359181c5
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-02-06 14:30:20 +0100

    redux

--- src/UserList.js
+++ src/UserList.js
@@ -1,5 +1,7 @@
 import {useEffect, useState} from "react";
 import API from "./api";
+import './style.css'
+import {Card} from "@material-ui/core";
 
 const UserList = ()=>{
     const [listOfUser,setListOfUser]=useState([])
@@ -14,8 +16,21 @@ const UserList = ()=>{
     },[])
 
     return(
-        <div>
+        <div className={'main'}>
             <h1>List user</h1>
+            <div className={'sous-main'}>
+                { listOfUser && listOfUser.map(user=>(
+                    <Card
+                        style={{backgroundColor:'#476eb7'}}
+                        className={'card'}>
+                        <h3>Name : {user.name}</h3>
+                        <h3>username : {user.username}</h3>
+                        <h3>email : {user.email}</h3>
+                        <h3>website : {user.website}</h3>
+                    </Card>
+                ))}
+            </div>
+
 
         </div>
     )
