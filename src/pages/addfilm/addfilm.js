4554f929659cfbc158e7c49ac3b3169c8ccfb6b8
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-02-12 12:05:32 +0100

    drrt

--- src/pages/addfilm/addfilm.js
+++ src/pages/addfilm/addfilm.js
@@ -1,10 +1,15 @@
 import './style.css'
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 const AddFilm = ()=>{
+    const location = useLocation();
+    const film = location.state ? location.state.params : '';
 return (
     <div>
-        <h1>Add Film </h1>
+        <h1>Details Film </h1>
+        <h3>Title : {film.title}</h3>
+        <h3>Type : {film.type}</h3>
+        <h3>Desctiption : {film.description}</h3>
         <br/>
         <Link to={'/listfilms'}>Go To List Films</Link>
     </div>
