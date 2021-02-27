84d80fa0dc85f1f1fb2ed8516de309e3ecd4a9c3
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-02-27 09:38:31 +0100

    uploadwithdata

--- src/pages/upload/upload.js
+++ src/pages/upload/upload.js
@@ -1,4 +1,4 @@
-import {Button, Card} from "@material-ui/core";
+import {Button, Card, TextField} from "@material-ui/core";
 import './style.css'
 import {useRef, useState} from "react";
 import API from "../../api/api";
@@ -8,6 +8,8 @@ const Upload = () => {
     const el = useRef();
     const [file, setFile] = useState('')
     const [src, setSrc] = useState('')
+    const [name, setName] = useState('')
+    const [cat, setCat] = useState('')
     const handel = (e) => {
         const ff = e.target.files[0]
         setFile(ff)
@@ -15,7 +17,9 @@ const Upload = () => {
     const send = () => {
         const formData = new FormData();
         formData.set('file', file);
-        API.post('upload', formData)
+        formData.set('name', name);
+        formData.set('cat', cat)
+        API.post('uploadwithdata', formData)
             .then(res => {
                 console.log(res.data)
                 setSrc(res.data.path)
@@ -28,6 +32,14 @@ const Upload = () => {
         <Card className={'main'}>
             <Card className={'card'}>
                 <h1>Uploader une Image</h1>
+                <TextField
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                    placeholder={'Name'}/>
+                <TextField
+                    value={cat}
+                    onChange={event => setCat(event.target.value)}
+                    placeholder={'Ctégorie'}/>
                 <input onChange={handel} ref={el} type={'file'}/>
                 <Button
                     style={{marginTop: 5}}
